deb80c2742266f93cd1ea637c28486fa666a4398
Grilo16 <98457056+Grilo16@users.noreply.github.com>
2022-08-07 14:53:38 +0100

    Added further tests to the loyalty status

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -16,7 +16,7 @@ class TestIntegration(unittest.TestCase):
         self.room3 = Room("Le Tiny Room", 1, 50)
 
         # Set up mock guests
-        self.dr_yan_nitor = Guest("Dr.Yan Nitor", 1000, self.rickRoll)
+        self.dr_yan_nitor = Guest("Dr.Yan Nitor", 10000, self.rickRoll)
         self.shanda_leer = Guest("Shanda Leer", 1000, self.gangnam)
         self.ben_dover = Guest("Ben Dover", 1000, self.friday)
         
@@ -74,7 +74,7 @@ class TestIntegration(unittest.TestCase):
     def test_wallets(self):
         self.assertEqual(900, self.shanda_leer.wallet)
         self.assertEqual(900, self.ben_dover.wallet)
-        self.assertEqual(900, self.dr_yan_nitor.wallet)
+        self.assertEqual(9900, self.dr_yan_nitor.wallet)
         
     # Test next time guests enter bars they are vips
     def test_vips(self):
@@ -108,9 +108,26 @@ class TestIntegration(unittest.TestCase):
         # Test wallets
         self.assertEqual(881, self.shanda_leer.wallet)
         self.assertEqual(886, self.ben_dover.wallet)
-        self.assertEqual(805, self.dr_yan_nitor.wallet)
+        self.assertEqual(9805, self.dr_yan_nitor.wallet)
 
         # Test tills
         self.assertEqual(119, self.room1.till)
         self.assertEqual(114, self.room2.till)
         self.assertEqual(195, self.room3.till)
+        
+    def test_loyalty_status(self):
+        
+        for _ in range(11):
+            self.ben_dover.enter_room(self.room1)
+            self.ben_dover.leave_room()
+        self.assertEqual("Bronze", self.room1.check_loyalty_status(self.ben_dover))
+        
+        for _ in range(51):
+            self.shanda_leer.enter_room(self.room1)
+            self.shanda_leer.leave_room()
+        self.assertEqual("Silver", self.room1.check_loyalty_status(self.shanda_leer))
+        
+        for _ in range(101):
+            self.dr_yan_nitor.enter_room(self.room1)
+            self.dr_yan_nitor.leave_room()
+        self.assertEqual("Gold", self.room1.check_loyalty_status(self.dr_yan_nitor))
